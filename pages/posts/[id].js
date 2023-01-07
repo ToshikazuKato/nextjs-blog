8a5ec41e2b85a53d00f4bd31b88cc229fa9c3f90
ToshikazuKato <kazu1427bsk@gmail.com>
2023-01-07 22:39:06 +1000

    dynamic routes

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,5 +1,8 @@
 import Layout from '../../components/layout';
 import { getAllPostIds, getPostData } from '../../lib/posts';
+import Head from 'next/head';
+import Date from '../../components/date';
+import unitStyles from '../../styles/utils.module.css';
 
 export async function getStaticProps({ params }) {
 	const postData = await getPostData(params.id);
@@ -21,13 +24,16 @@ export async function getStaticPaths() {
 export default function Post( { postData } ){
 	return (
 		<Layout>
-			{postData.title}
-			<br />
-			{postData.id}
-			<br />
-			{postData.date}
-			<br />
-			<div dangerouslySetInnerHTML={ { __html:postData.contentHtml } }></div>
+			<Head>
+				<title>{postData.title}</title>
+			</Head>
+			<article>
+				<h1 className={unitStyles.headingXl}>{postData.title}</h1>
+				<div className={unitStyles.lightText}>
+					<Date dateString={postData.date} />
+				</div>
+				<div dangerouslySetInnerHTML={{ __html: postData.contentHtml }}></div>
+			</article>
 		</Layout>
 	);
 }
